edf4543e5003b1cff4d2dda059e93121d6d16ad2
Elif Gökpınar <72789565+elifgokpinar@users.noreply.github.com>
2022-04-06 13:06:16 +0300

    Update test_command.cmd

--- Generate Test Commands/test_command.cmd
+++ Generate Test Commands/test_command.cmd
@@ -1,1 +1,5 @@
-
+python test.py --image masked_inputs/1.png --mask masks/1.png --output outputs/output_1.png --checkpoint_dir logs/full_model_celeba_hq_256
+python test.py --image masked_inputs/2.png --mask masks/2.png --output outputs/output_2.png --checkpoint_dir logs/full_model_celeba_hq_256
+python test.py --image masked_inputs/3.png --mask masks/3.png --output outputs/output_3.png --checkpoint_dir logs/full_model_celeba_hq_256
+python test.py --image masked_inputs/4.png --mask masks/4.png --output outputs/output_4.png --checkpoint_dir logs/full_model_celeba_hq_256
+python test.py --image masked_inputs/5.png --mask masks/5.png --output outputs/output_5.png --checkpoint_dir logs/full_model_celeba_hq_256
